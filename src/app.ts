f2b9770da83aca3d184546b31a45c47a32e9035a
Braden Tiernan <112308003+Braden-Tiernan@users.noreply.github.com>
2024-08-30 11:04:02 -0500

    started bot setup uwu

--- src/app.ts
+++ src/app.ts
@@ -8,6 +8,29 @@ import { my_function } from "libs/mylib";
 //TODO: it will be way easier to update the database structure with django, but way more complicated to setup.
 
 //todo: setup bot
+import TelegramBot from "node-telegram-bot-api";
+require("dotenv").config();
+
+// replace the value below with the Telegram token you receive from @BotFather
+const token = process.env.TELEGRAM_BOT_TOKEN;
+console.log(token);
+
+// Create a bot that uses 'polling' to fetch new updates
+const bot = new TelegramBot(token, {polling: true});
+
+// Matches "/echo [whatever]"
+bot.onText(/\/echo (.+)/, (msg, match) => {
+    // 'msg' is the received Message from Telegram
+    // 'match' is the result of executing the regexp above on the text content
+    // of the message
+  
+    const chatId = msg.chat.id;
+    const resp = match[1]; // the captured "whatever"
+  
+    // send back the matched "whatever" to the chat
+    console.log(resp);
+    bot.sendMessage(chatId, resp);
+  });
 
 //todo: setup bot "/start" event handler. It should add the user to the users database and then tell the
 //todo: user to send a sticker to tag
