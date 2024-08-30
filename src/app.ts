fadc1ae23b614092227c5c779a4d4c977f10c8d4
Braden Tiernan <112308003+Braden-Tiernan@users.noreply.github.com>
2024-08-30 11:04:36 -0500

    started bot setup uwu

--- src/app.ts
+++ src/app.ts
@@ -18,20 +18,6 @@ console.log(token);
 // Create a bot that uses 'polling' to fetch new updates
 const bot = new TelegramBot(token, {polling: true});
 
-// Matches "/echo [whatever]"
-bot.onText(/\/echo (.+)/, (msg, match) => {
-    // 'msg' is the received Message from Telegram
-    // 'match' is the result of executing the regexp above on the text content
-    // of the message
-  
-    const chatId = msg.chat.id;
-    const resp = match[1]; // the captured "whatever"
-  
-    // send back the matched "whatever" to the chat
-    console.log(resp);
-    bot.sendMessage(chatId, resp);
-  });
-
 //todo: setup bot "/start" event handler. It should add the user to the users database and then tell the
 //todo: user to send a sticker to tag
 
