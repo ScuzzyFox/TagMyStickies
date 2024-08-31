5482a97ee145496af1b21c7359c059712ba1c725
Braden Tiernan <112308003+Braden-Tiernan@users.noreply.github.com>
2024-08-31 15:04:26 -0500

    start responses updates
    I updated and added some new responses when using /start

--- src/libs/eventHandlers/handlers.ts
+++ src/libs/eventHandlers/handlers.ts
@@ -1,12 +1,26 @@
 const startResponses = [ // Feel free to think up some more responses! :D (or even edit mine uwu)
   [{message:"Send me a sticker to tag!",time:0}],
-  [{message:"OwO, thanks for talking to me!\n\nSend me a sicker and we can get started!",time:0}],
+  [{message:"OwO, thanks for talking to me!\n\nSend me a sticker and we can get started!",time:0}],
   [{message:"o3o, sup! I was just uhh... nvm. Send me a sticker to get started!",time:0}],
   [
     {message:"Gah! Heck.... One second buddy!...",time:0},
     {message:"...",time:2000},
     {message:"Ok! I'm here! You wanna send me a sticker?",time:4000}
-  ]
+  ],
+  [{message:"Oh, you need my assistance? You've got a sticker to show me, right? *wags tail*",time:0}],
+  [{message:"You have my undivided attention! I can't wait to see which sticker you'll send me!",time:0}],
+  [
+    {message:"Hm? You say you have something you'd like to show me~?",time:0},
+    {message:"A-ah... r-right, a sticker! Send it here, please, and we'll see what we can do! ;3",time:2000}
+  ],
+  [{message:"UwU, hewwos! You wanna tag stickers, huh?~ Just send one here!",time:0}],
+  [{message:"Need my help? Just send me a sticker, and I'll do my best to assist!",time:0}],
+  [
+    {message:"I just can't stand being without new stickers... >n<",time:0},
+    {message:"Hurry up and show me one of your stickers already! I'm begging you! >~<",time:3000}
+  ],
+  [{message:"Doesn't a day like today make you want to tag stickers (with the help of your favorite sticker-loving companion)?~ Send me a sticker!",time:0}],
+  [{message:"H-hurry up and put it in me already... ~.~",time:0}],
 ];
 var startEventHandlerscv1 = {
   responses:{
@@ -20,7 +34,7 @@ export function startEventHandlers(bot) {
         //TODO: save chatID to the database
         //TODO: set the user interaction state to accepting a sticker
 
-        //! I tried to user msg.user.id and get a polling error, suggesting that either the syntax is wrong or my key doesn't allow me to get the user ID.
+        //! I tried to use msg.user.id and got a polling error, suggesting that either the syntax is wrong or my key doesn't allow me to get the user ID.
         // msg.user.id (get the user ID)
 
 
