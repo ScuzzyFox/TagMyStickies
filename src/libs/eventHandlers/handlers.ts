94d78a239c44fdfbae3601ff99b22461cb05a55b
Braden Tiernan <112308003+Braden-Tiernan@users.noreply.github.com>
2024-08-31 12:50:31 -0500

    New /start Responses!
    Added a function to check the user’s interaction state with the bot.
    
    Experamented with getting the user id and noted a polling error.
    
    Added some fun /start responses, and made them random!

--- src/libs/eventHandlers/handlers.ts
+++ src/libs/eventHandlers/handlers.ts
@@ -1,16 +1,54 @@
+const startResponses = [ // Feel free to think up some more responses! :D (or even edit mine uwu)
+  [{message:"Send me a sticker to tag!",time:0}],
+  [{message:"OwO, thanks for talking to me!\n\nSend me a sicker and we can get started!",time:0}],
+  [{message:"o3o, sup! I was just uhh... nvm. Send me a sticker to get started!",time:0}],
+  [
+    {message:"Gah! Heck.... One second buddy!...",time:0},
+    {message:"...",time:2000},
+    {message:"Ok! I'm here! You wanna send me a sticker?",time:4000}
+  ]
+];
+var startEventHandlerscv1 = {
+  responses:{
+    avalResp: Array.from({ length: startResponses.length }, (_, i) => i)
+  }
+};
 export function startEventHandlers(bot) {
     bot.onText(/\/start/, (msg) => {
       
         //TODO: add user to the database
         //TODO: save chatID to the database
+        //TODO: set the user interaction state to accepting a sticker
+
+        //! I tried to user msg.user.id and get a polling error, suggesting that either the syntax is wrong or my key doesn't allow me to get the user ID.
         // msg.user.id (get the user ID)
 
 
         const chatId = msg.chat.id;
         
-        const resp = "Semd me a sticker to tag!";
-      
-        bot.sendMessage(chatId, resp);
+        // gets the id of a random response!
+        if (startEventHandlerscv1.responses.avalResp.length<1) {
+          startEventHandlerscv1.responses.avalResp=Array.from({ length: startResponses.length }, (_, i) => i);
+        }
+        var randomaval = Math.round(Math.random()*(startEventHandlerscv1.responses.avalResp.length-1));
+        var randomID = startEventHandlerscv1.responses.avalResp[randomaval];
+        startEventHandlerscv1.responses.avalResp.splice(randomaval, 1);
+        console.log(randomID, startEventHandlerscv1.responses.avalResp)
+        /*while(startEventHandlerscv1.lastStartResp==randomID) {
+          var randomID = Math.round(Math.random()*(startResponses.length-1));
+        }
+        startEventHandlerscv1.lastStartResp = randomID;*/
+        
+        // sends the responses!
+        startResponses[randomID].forEach((a, b) => {
+          setTimeout(() => {
+            bot.sendMessage(chatId, a.message);
+          }, a.time);
+        });
 
       });
+}
+
+function getUserState(userID) {
+    //TODO: get user state from database from the user's ID
 }
