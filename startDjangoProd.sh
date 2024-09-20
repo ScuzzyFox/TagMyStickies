2beb27f2562dbf90cbd70202c52ae973e84d2bfa
scuzzyfox <scuzzyfox@gmail.com>
2024-09-19 22:59:51 -0600

    tyring to fix error

---
+++ startDjangoProd.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+export DJANGO_SETTINGS_MODULE=tagmystickies.settings
+cd Django
+source env/bin/activate
+pip install -r requirements.txt
+cd tagmystickies
+python manage.py makemigrations
+python manage.py migrate
+hypercorn tagmystickies.asgi:application
